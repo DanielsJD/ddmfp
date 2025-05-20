1f0dd2f359e5664954af9822d72b05dd45d134a6
DanielsJD <daniele.loliva@gmail.com>
2025-05-20 21:17:55 +0200

    Update onlyevents.py

--- onlyevents.py
+++ onlyevents.py
@@ -9,8 +9,8 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://danielsjd-mickeyomg.hf.space"     # non mettere lo / finale al link
+MFPPSW = "DaX9091"
 
 # Costanti
 NUM_CHANNELS = 10000
